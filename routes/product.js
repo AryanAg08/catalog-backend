734d80d3aa64c290a635c554caf3c9a73f999e17
AryanAg08 <aryangoyal90@outlook.com>
2024-03-06 20:42:51 +0530

    V2.1

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,6 @@
-import express from "express";
-import { addPost, getProduct} from "../controllers/product.js"
+const express = require ("express");
+const { addPost, getProduct} = require("../controllers/product.js");
 const router=express.Router();
 router.get("/",getProduct)
 router.post("/addproduct",addPost)
-export default router;
+module.exports = router;
