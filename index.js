9e50590ab843310725930b9079e319b60848fa72
AryanAg08 <aryangoyal90@outlook.com>
2024-03-07 06:46:02 +0530

    Adding csv

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.use("/api/csv", upload2.single("file"), (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: 'No file uploaded' });
   }
-   console.log("file recieved!!");
+   console.log("file recieved!!", req.file);
 
    const results = [];
    const filename = fileuploadpath;
